db961707d6563c6b0741d9d7065d1e6453d6d9c3
fatkulnurk <fatkulnurk@gmail.com>
2019-10-12 22:09:33 +0700

    first commit

--- config.php
+++ config.php
@@ -12,6 +12,25 @@ $config = [
 function getQRCODE($config) {
     print sprintf('<img src="%s"/>',TokenAuth6238::getBarCodeUrl($config['username'], $config['domain'], $config['secret_key'], $config['issuer']));
 }
+// Function to get the client IP address
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
 
 function login_invalid($username = 'Username'){
     header('Content-Type: text/html; charset=utf-8');
@@ -19,8 +38,8 @@ function login_invalid($username = 'Username'){
 
     // write login failed in logfile for fail2ban
     $dat = date('M j H:i:s Y');
-    $error_essage = "Authentication failed for: $username";
-    $client_ip = $_SERVER['HTTP_CLIENT_IP']?:($_SERVER['HTTP_X_FORWARDE‌​D_FOR']?:$_SERVER['REMOTE_ADDR']);
-	error_log("[$dat] [WARNING] [client: $client_ip] $error_essage\n", 3);
+    $error_message = "Authentication failed for: $username";
 
+    $client_ip = get_client_ip();
+	error_log("[$dat] [WARNING] [client: $client_ip] $error_message\n");
 }
